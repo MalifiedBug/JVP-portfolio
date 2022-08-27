49e46a26f42bcc5fc90dcd1a30792beb4fa50de3
MalifiedBud <109272093+MalifiedBug@users.noreply.github.com>
2022-08-27 23:01:19 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-async function fetchimg(){
-    await fetch("https://media-exp1.licdn.com/dms/image/C4D03AQGPqr6IhLkwrg/profile-displayphoto-shrink_800_800/0/1646638498682?e=2147483647&v=beta&t=pX8s3Au7SLK-V1uQDIkrKsqXJkCbvQYBi066gSyDz40")
-    .then((res)=>document.write(res))
-}
